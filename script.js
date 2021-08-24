af8b97039e527c764fdaab01db51759b2cd3087d
kevduc <kduconge@gmail.com>
2021-08-24 21:10:15 +0200

    Correct assignment of card images (2 of each only, for pairs)

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min
+}
+
+const fetchJSON = async (url) => await (await fetch(url)).json()
+
+const randUniq = (n) => {
+  const arr = new Array(n).fill().map((_, i) => i)
+
+  for (let i = 0; i < n - 1; i++) {
+    const randIndex = randInt(0, n - i - 1)
+    ;[arr[i], arr[randIndex]] = [arr[randIndex], arr[i]]
+  }
+
+  return arr
+}
+
 const numCards = 24
 const imageSize = 500
 
 const numImages = numCards / 2
 const numImagesMax = 100
-const pageNum = Math.floor(Math.floor(numImagesMax / numImages) * Math.random()) + 1
-
-const fetchJSON = async (url) => await (await fetch(url)).json()
+const pageNum = randInt(1, Math.floor(numImagesMax / numImages))
 
 document.addEventListener('DOMContentLoaded', async () => {
   const images = await fetchJSON(`https://picsum.photos/v2/list?page=${pageNum}&limit=${numImages}`)
@@ -17,19 +32,28 @@ document.addEventListener('DOMContentLoaded', async () => {
   const cardContainer = card.parentElement
 
   let i = 0
-  while (true) {
-    card.addEventListener('click', function () {
-      document.querySelector('.card--selected')?.classList.remove('card--selected')
-      this.classList.add('card--selected')
-      this.classList.toggle('card--flipped')
-    })
 
-    const cardBack = card.querySelector('.card__face--back')
-    cardBack.style.backgroundImage = `url('https://picsum.photos/seed/${i + 1}/500')`
+  while (true) {
+    card.addEventListener('click', cardClick)
 
     if (++i === numCards) break
 
     card = card.cloneNode(true)
     cardContainer.append(card)
   }
+
+  const cardBacks = document.querySelectorAll('.card__face--back')
+  const randomIndexes = randUniq(numCards)
+
+  for (let i = 0; i < randomIndexes.length; i++) {
+    const cardBack = cardBacks[randomIndexes[i]]
+    const imageIndex = Math.floor(i / 2)
+    cardBack.style.backgroundImage = `url('${images[imageIndex].download_url}')`
+  }
 })
+
+function cardClick() {
+  document.querySelector('.card--selected')?.classList.remove('card--selected')
+  this.classList.add('card--selected')
+  this.classList.toggle('card--flipped')
+}
